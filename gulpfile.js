27fa9fb28ff1a34722591142e7488fbfd12d8d96
geekkeef <keithtjunior@gmail.com>
2017-11-24 04:31:16 -0500

    major updates + nav creation

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ autoprefixer        = require('autoprefixer'),
 cssvars             = require('postcss-simple-vars'),
 cssnested           = require('postcss-nested'),
 cssimport           = require('postcss-import'),
-browserSync         = require('browser-sync').create();        
+browserSync         = require('browser-sync').create(),
+hexrgba             = require('postcss-hexrgba');        
 
 gulp.task('default', function(){        // gulp needs default task
     console.log('GULP DEFAULT TASK');
@@ -18,7 +19,7 @@ gulp.task('html', function(){           // reload browser when html updates
 
 gulp.task('styles', function(){     // pipe css file through post-css filters to temp dir
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssimport, cssvars, cssnested, autoprefixer]))      // postcss + autoprefixer create vendor prefixes for css     
+    .pipe(postcss([cssimport, cssvars, cssnested, hexrgba, autoprefixer]))      // postcss + autoprefixer create vendor prefixes for css     
     .pipe(gulp.dest('./app/temp/styles'));              // cssimport will replace @import with text from css styles (modules)
 });                                                 // cssvars lets you use variables in css
                                                     // cssnested lets you nest css styles (ie SASS)
